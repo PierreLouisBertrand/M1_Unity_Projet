b75ca92fdec513cc35c471f25729e9af114b3339
Pierre-Louis Bertrand <pierrelouis.bertrand@gmail.com>
2020-12-21 19:59:57 +0100

    la caméra suit le joueur

---
+++ Assets/Scripts/CenteringCameraOnPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenteringCameraOnPlayer : MonoBehaviour
+{
+
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPosition = player.GetComponent<Transform>().position;
+        playerPosition.z = -1;
+        transform.position = playerPosition;
+    }
+}
