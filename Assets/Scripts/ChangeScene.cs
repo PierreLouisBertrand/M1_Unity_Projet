3feb230415964bfc7c8f91ab354cf10158084b25
Pierre-Louis Bertrand <pierrelouis.bertrand@gmail.com>
2020-12-22 23:39:38 +0100

    Le bouton 'Jouer' fonctionne

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    public void goToMainMenu()
+    {
+        SceneManager.LoadScene("01 - MainMenu");
+    }
+
+    public void goToGame()
+    {
+        SceneManager.LoadScene("New Scene");
+    }
+    
+}
