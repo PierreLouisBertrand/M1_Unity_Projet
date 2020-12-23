2514b00247c4dd8fa9d0875a47ba47dc8dcb00a9
Pierre-Louis Bertrand <pierrelouis.bertrand@gmail.com>
2020-12-23 18:28:38 +0100

    Ajout prefab Ennemi avec script de mouvement pour suivre le joueur si il est proche

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    private SpriteRenderer _spriteRenderer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        Vector3 playerPosition = player.GetComponent<Transform>().position;
+        Vector3 enemyPosition = transform.position;
+        playerPosition.z = enemyPosition.z; // puisque le jeu est en 2D, on s'assure que les calculs suivants ne prendront pas en compte la profondeur
+        float distance = Vector3.Distance(playerPosition, enemyPosition);
+        if (distance < 5f)
+        {
+            transform.position = Vector3.MoveTowards(enemyPosition, playerPosition, 0.01f);
+            if (playerPosition.x > enemyPosition.x)
+            {
+                _spriteRenderer.flipX = true;
+            }
+            else
+            {
+                _spriteRenderer.flipX = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance;
+    private bool isPaused = false;
+
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance.gameObject);
+        }
+        else if (this != instance)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPaused)
+        {
+            Time.timeScale = 0f;
+        }
+    }
+
+    public void pause()
+    {
+        this.isPaused = true;
+    }
+
+    public void resume()
+    {
+        this.isPaused = false;
+        Time.timeScale = 1f;
+    }
+}
