8761632b18bbc54f08f8af9934b48b6f95b84986
Pierre-Louis Bertrand <pierrelouis.bertrand@gmail.com>
2020-12-27 18:03:08 +0100

    L'ennemi peut perdre de la vie (affiché sur sa barre de vie), et quand il en a plus il disparait

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float maximumHealth;
+    private float _health;
+    public Image healthBar;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _health = maximumHealth;
+        healthBar.fillAmount = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TakeDamage(1f);
+    }
+
+    void TakeDamage(float amount)
+    {
+        _health -= amount;
+        UpdateHealthBar();
+        if (_health <= 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void UpdateHealthBar()
+    {
+        healthBar.fillAmount = _health / maximumHealth;
+    }
+    
+    
+}
